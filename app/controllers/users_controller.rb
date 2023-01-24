027d2821426f70f59a7aa12944cd01a321160431
miurachikaa <chikaa719@gmail.com>
2023-01-24 17:08:01 +0900

    修正プッシュ③

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, except: [:show]
   def show
     @user = User.find(params[:id])
-    @prototype = Prototype.find(params[:id])
-  end
+    @prototypes = @user.prototypes.includes(:user)
+    end
 end
